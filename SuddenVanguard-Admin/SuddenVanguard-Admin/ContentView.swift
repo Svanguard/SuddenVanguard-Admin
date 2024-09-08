58f9e9b6fee44e10423bca133162232f203ec701
DongHo Choe <hamfan524@naver.com>
2024-09-09 00:31:32 +0900

    add: 프로젝트 파일 생성

---
+++ SuddenVanguard-Admin/SuddenVanguard-Admin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuddenVanguard-Admin
+//
+//  Created by 최동호 on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuddenVanguard-Admin/SuddenVanguard-Admin/SuddenVanguard_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuddenVanguard_AdminApp.swift
+//  SuddenVanguard-Admin
+//
+//  Created by 최동호 on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SuddenVanguard_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
